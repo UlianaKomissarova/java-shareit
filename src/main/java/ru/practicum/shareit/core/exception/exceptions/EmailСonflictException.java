3f0e84f148df4afb1e19fc035448708d770af5c0
Ulyana <ulyana_vas@mail.ru>
2023-10-06 19:10:44 +0300

    fix: удаление лишнего класса

--- src/main/java/ru/practicum/shareit/core/exception/exceptions/EmailСonflictException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.core.exception.exceptions;
-
-public class EmailСonflictException extends ConflictException {
-    public EmailСonflictException(String message) {
-        super(message);
-    }
-}
